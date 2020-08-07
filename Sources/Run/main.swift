7d79f7a16a8299611f4abad750f1243cf842f8a7
Alan Chu <ualch9@gmail.com>
2020-08-06 21:42:00 -0700

    Adds mongo dependencies

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,16 @@
 import App
 import Vapor
+import Fluent
+import FluentMongoDriver
+
+guard let mongoDBURI = ProcessInfo.processInfo.environment["MONGO_DB_URI"] else {
+    fatalError("Missing ENV \"MONGO_DB_URI\"")
+}
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
+try app.databases.use(.mongo(connectionString: mongoDBURI), as: .mongo)
 try app.run()
